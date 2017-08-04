da8e29f5f5d252840eaf5ea042afeb6b8c0adc16
Tanvir Hossain <thossain@icddrb.org>
2017-08-04 12:03:23 +0600

    Tanvir

--- app/src/main/java/org/icddrb/standard/LoginActivity.java
+++ app/src/main/java/org/icddrb/standard/LoginActivity.java
@@ -26,7 +26,7 @@ import Common.Connection;
 import Common.Global;
 import Common.ProjectSetting;
 import Utility.MySharedPreferences;
-//test push
+//test push bitbucket
 public class LoginActivity extends Activity {
     public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
     Connection C;
